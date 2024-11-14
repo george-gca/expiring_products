716f088394c684888cb8af857d7745632eedad82
George Araújo <george.gcac@gmail.com>
2024-11-14 11:04:24 -0300

    Added services worker for offline usage, not exporting when nothing to export
    Signed-off-by: George Araújo <george.gcac@gmail.com>

---
+++ assets/js/sw.js
@@ -0,0 +1,39 @@
+const cacheName = 'expiring-products-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/en-us/index.html',
+        '/assets/js/idb-backup-and-restore.mjs',
+        '/assets/img/favicon.png',
+        '/assets/img/favicon_colored.png'
+        // '/style.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key === cacheName) {
+            return;
+          }
+          return caches.delete(key);
+        }),
+      );
+    }),
+  );
+});
+
+self.addEventListener('fetch', e => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
